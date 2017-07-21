edf042d57adfaa655cc66e8ea67d23536696dab0
Bradley J. Clement <bclement@jpl.nasa.gov>
2017-07-20 17:46:24 -0700

    fixed something

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express')
 const app = express()
 
+const { exec } = require('child_process');
+
 app.use(express.static('editor'))
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
+app.post('/solve', function (req, res) {
+    req.body
+  exec('k "' + req.body + '"', (error, stdout, stderr) => {
+      if (error) {
+              console.error(`exec error: ${error}`);
+                  return;
+                    }
+       res.send(`stdout: ${stdout}`);
+  });
+
+})
+
