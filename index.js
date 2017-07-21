4344a91de2f2764cb90a8f68e5f1ebb066f0b9cc
afrivera <Andres.F.Rivera@jpl.nasa.gov>
2017-07-20 17:18:00 -0700

    restructured project added nodejs basic server

--- index.js
+++
@@ -1,9 +0,0 @@
-var editor = ace.edit('editor');
-ace.require("ace/ext/language_tools");
-editor.setTheme("ace/theme/twilight");
-editor.getSession().setMode('ace/mode/k-mode');
-editor.setOption("showPrintMargin", false);
-editor.setOptions({
-    enableBasicAutocompletion: true,
-    // enableLiveAutocompletion: true
-});
