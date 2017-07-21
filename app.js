f56a059725116cd687a8b21a33a3bdf7f6308acb
afrivera <Andres.F.Rivera@jpl.nasa.gov>
2017-07-20 18:16:46 -0700

    solve in clinet

--- app.js
+++ app.js
@@ -3,21 +3,29 @@ const app = express()
 const bodyParser = require('body-parser');
 const { exec } = require('child_process');
 
+
 app.use(express.static('editor'))
-app.use(bodyParser());
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
 
 app.post('/solve', function (req, res) {
-  exec('k "' + req.body + '"', (error, stdout, stderr) => {
+  if(typeof req.body.value !== 'undefined'){
+    exec('k "' + req.body.value + '"', (error, stdout, stderr) => {
     if (error) {
       console.error(`exec error: ${error}`);
-      return;
+      res.send('error');
     }
-    res.send(`stdout: ${stdout}`);
+    res.send(`Result: ${stdout}`);
   });
+  }
 
 })
 
+ 
--- editor/index.js
+++ editor/index.js
@@ -7,3 +7,8 @@ editor.setOptions({
     enableBasicAutocompletion: true,
     // enableLiveAutocompletion: true
 });
+ $('#send').on('click', function (){
+         $.post('http://localhost:3000/solve',{value: editor.getValue()} , function (data){
+           $('#solution').html(data);
+         }) 
+    });
