687136f90425ce76851eff0b242beb8d2fd9deda
afrivera <Andres.F.Rivera@jpl.nasa.gov>
2017-07-20 17:53:05 -0700

    added body parser

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 const express = require('express')
 const app = express()
-
+const bodyParser = require('body-parser');
 const { exec } = require('child_process');
 
 app.use(express.static('editor'))
+app.use(bodyParser());
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
 
 app.post('/solve', function (req, res) {
-    req.body
   exec('k "' + req.body + '"', (error, stdout, stderr) => {
-      if (error) {
-              console.error(`exec error: ${error}`);
-                  return;
-                    }
-       res.send(`stdout: ${stdout}`);
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    res.send(`stdout: ${stdout}`);
   });
 
 })
