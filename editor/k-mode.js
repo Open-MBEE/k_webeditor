00b390de5db7da99e86133168633be9f433966fe
afrivera <Andres.F.Rivera@jpl.nasa.gov>
2017-07-30 13:16:11 -0700

    interactivity changes

--- editor/k-mode.js
+++ editor/k-mode.js
@@ -550,6 +550,9 @@ ace.define(
     ],
     function (require, exports, module) {
         var oop = require("ace/lib/oop");
+        var lang = require("../lib/lang");
+        var Range = require("../range").Range;
+        var TokenIterator = require("../token_iterator").TokenIterator;
         var TextMode = require("ace/mode/text").Mode;
         var tokenTypeMapping = antlr4_require('./k-token-map');
         var createTokenTypeMap = require('ace/ext/antlr4/token-type-map').createTokenTypeMap;
@@ -572,8 +575,192 @@ ace.define(
         (function () {
 
             this.$id = "ace/mode/k-mode";
-            this.lineCommentStart = "//";
+            this.lineCommentStart = "---";
             this.blockComment = { start: "===", end: "===" };
+
+            this.toggleCommentLines = function(state, session, startRow, endRow) {
+                var doc = session.doc;
+
+                var ignoreBlankLines = true;
+                var shouldRemove = true;
+                var minIndent = Infinity;
+                var tabSize = session.getTabSize();
+                var insertAtTabStop = false;
+
+                if (!this.lineCommentStart) {
+                    if (!this.blockComment)
+                        return false;
+                    var lineCommentStart = this.blockComment.start;
+                    var lineCommentEnd = this.blockComment.end;
+                    var regexpStart = new RegExp("^(\\s*)(?:" + lang.escapeRegExp(lineCommentStart) + ")");
+                    var regexpEnd = new RegExp("(?:" + lang.escapeRegExp(lineCommentEnd) + ")\\s*$");
+
+                    var comment = function(line, i) {
+                        if (testRemove(line, i))
+                            return;
+                        if (!ignoreBlankLines || /\S/.test(line)) {
+                            doc.insertInLine({row: i, column: line.length}, lineCommentEnd);
+                            doc.insertInLine({row: i, column: minIndent}, lineCommentStart);
+                        }
+                    };
+
+                    var uncomment = function(line, i) {
+                        var m;
+                        if (m = line.match(regexpEnd))
+                            doc.removeInLine(i, line.length - m[0].length, line.length);
+                        if (m = line.match(regexpStart))
+                            doc.removeInLine(i, m[1].length, m[0].length);
+                    };
+
+                    var testRemove = function(line, row) {
+                        if (regexpStart.test(line))
+                            return true;
+                        var tokens = session.getTokens(row);
+                        for (var i = 0; i < tokens.length; i++) {
+                            if (tokens[i].type === "comment")
+                                return true;
+                        }
+                    };
+                } else {
+                    if (Array.isArray(this.lineCommentStart)) {
+                        var regexpStart = this.lineCommentStart.map(lang.escapeRegExp).join("|");
+                        var lineCommentStart = this.lineCommentStart[0];
+                    } else {
+                        var regexpStart = lang.escapeRegExp(this.lineCommentStart);
+                        var lineCommentStart = this.lineCommentStart;
+                    }
+                    regexpStart = new RegExp("^(\\s*)(?:" + regexpStart + ") ?");
+
+                    insertAtTabStop = session.getUseSoftTabs();
+
+                    var uncomment = function(line, i) {
+                        var m = line.match(regexpStart);
+                        if (!m) return;
+                        var start = m[1].length, end = m[0].length;
+                        if (!shouldInsertSpace(line, start, end) && m[0][end - 1] == " ")
+                            end--;
+                        doc.removeInLine(i, start, end);
+                    };
+                    var commentWithSpace = lineCommentStart + " ";
+                    var comment = function(line, i) {
+                        if (!ignoreBlankLines || /\S/.test(line)) {
+                            if (shouldInsertSpace(line, minIndent, minIndent))
+                                doc.insertInLine({row: i, column: minIndent}, commentWithSpace);
+                            else
+                                doc.insertInLine({row: i, column: minIndent}, lineCommentStart);
+                        }
+                    };
+                    var testRemove = function(line, i) {
+                        return regexpStart.test(line);
+                    };
+
+                    var shouldInsertSpace = function(line, before, after) {
+                        var spaces = 0;
+                        while (before-- && line.charAt(before) == " ")
+                            spaces++;
+                        if (spaces % tabSize != 0)
+                            return false;
+                        var spaces = 0;
+                        while (line.charAt(after++) == " ")
+                            spaces++;
+                        if (tabSize > 2)
+                            return spaces % tabSize != tabSize - 1;
+                        else
+                            return spaces % tabSize == 0;
+                        return true;
+                    };
+                }
+
+                function iter(fun) {
+                    for (var i = startRow; i <= endRow; i++)
+                        fun(doc.getLine(i), i);
+                }
+
+
+                var minEmptyLength = Infinity;
+                iter(function(line, i) {
+                    var indent = line.search(/\S/);
+                    if (indent !== -1) {
+                        if (indent < minIndent)
+                            minIndent = indent;
+                        if (shouldRemove && !testRemove(line, i))
+                            shouldRemove = false;
+                    } else if (minEmptyLength > line.length) {
+                        minEmptyLength = line.length;
+                    }
+                });
+
+                if (minIndent == Infinity) {
+                    minIndent = minEmptyLength;
+                    ignoreBlankLines = false;
+                    shouldRemove = false;
+                }
+
+                if (insertAtTabStop && minIndent % tabSize != 0)
+                    minIndent = Math.floor(minIndent / tabSize) * tabSize;
+
+                iter(shouldRemove ? uncomment : comment);
+            };
+
+            this.toggleBlockComment = function(state, session, range, cursor) {
+                var comment = this.blockComment;
+                if (!comment)
+                    return;
+                if (!comment.start && comment[0])
+                    comment = comment[0];
+
+                var iterator = new TokenIterator(session, cursor.row, cursor.column);
+                var token = iterator.getCurrentToken();
+
+                var sel = session.selection;
+                var initialRange = session.selection.toOrientedRange();
+                var startRow, colDiff;
+
+                if (token && /comment/.test(token.type)) {
+                    var startRange, endRange;
+                    while (token && /comment/.test(token.type)) {
+                        var i = token.value.indexOf(comment.start);
+                        if (i != -1) {
+                            var row = iterator.getCurrentTokenRow();
+                            var column = iterator.getCurrentTokenColumn() + i;
+                            startRange = new Range(row, column, row, column + comment.start.length);
+                            break;
+                        }
+                        token = iterator.stepBackward();
+                    }
+
+                    var iterator = new TokenIterator(session, cursor.row, cursor.column);
+                    var token = iterator.getCurrentToken();
+                    while (token && /comment/.test(token.type)) {
+                        var i = token.value.indexOf(comment.end);
+                        if (i != -1) {
+                            var row = iterator.getCurrentTokenRow();
+                            var column = iterator.getCurrentTokenColumn() + i;
+                            endRange = new Range(row, column, row, column - comment.end.length);
+                            break;
+                        }
+                        token = iterator.stepForward();
+                    }
+                    if (endRange)
+                        session.remove(endRange);
+                    if (startRange) {
+                        session.remove(startRange);
+                        startRow = startRange.start.row;
+                        colDiff = -comment.start.length;
+                    }
+                } else {
+                    colDiff = comment.start.length;
+                    startRow = range.start.row;
+                    session.insert(range.end, comment.end);
+                    session.insert(range.start, comment.start);
+                }
+                // todo: selection should have ended up in the right place automatically!
+                // if (initialRange.start.row == startRow)
+                //     initialRange.start.column += colDiff;
+                // if (initialRange.end.row == startRow)
+                //     initialRange.end.column += colDiff;
+                // session.selection.fromOrientedRange(initialRange);
+            };
 
             this.getTokenizer = function () {
                 if (!this.$tokenizer) {
@@ -646,18 +833,22 @@ ace.define(
                         }
                     };
                     const toTex = p => {p.value = p.value.toTex(); return p;};
+                    const toExprDOM = p => p.children ? cardOut(p) : $('<p class="prettyExpr" data-line="'+p.line+'" data-col="'+p.col+'">$$'+p.value+'$$</p>');
                     const cardOut = (obj) => {
-                        return obj.children.length > 0 ? `
- <div class="ui fluid accordion segment">
-    <div class="active title"> <i class="chevron down icon"></i> ${obj.name}</div>
-    <div class="active content">
-    <i>${obj.children.length} expressions</i>
-    ${obj.children.map(stripReq).map(parseMath).map(toTex).map(p=>'<p class="prettyExpr" data-line="'+p.line+'" data-col="'+p.col+'">$$'+p.value+'$$</p>').join('')}
-    </div>
-  </div>
-  <div class="title"></div>
-  <div class="content"></div>
-</div>` : ''};
+                        if(obj.children.length == 0){
+                            return '';
+                        }
+                        var card = $('<div class="ui fluid accordion segment"></div>');
+                        var cardTitle =  $(`<div class="active title"> <i class="chevron down icon"></i> ${obj.name}</div>`);
+                        var cardContent = $(`<div class="active content"></div>`);
+                        cardContent.append(`<i>${obj.children.length} expressions</i>`);
+                        let expressions = obj.children.map(stripReq).map(parseMath).map(toTex).map(toExprDOM);
+                        cardContent.append(expressions);
+                        card.append(cardTitle);
+                        card.append(cardContent);
+                        return card;
+                    };
+
                     const goToLineandCol = function (e){
                         var editor = ace.edit('editor');
                         let l = $(this).data('line');
@@ -670,9 +861,8 @@ ace.define(
                     let expStr = e.data.map(cardOut);
 
                     $('#renderDiv').html(expStr);
-                    $('.ui.accordion').accordion();
-
-                    $('.prettyExpr').on('click', goToLineandCol);
+                    $('#renderDiv .ui.accordion').accordion();
+                    $('#renderDiv .prettyExpr').on('click', goToLineandCol);
 
                     MathJax.Hub.Queue(["Typeset", MathJax.Hub,"renderDiv"]);
                 });
