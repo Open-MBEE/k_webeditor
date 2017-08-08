a7cbf9361c501b0af4a1f8aa91e54d792622452e
afrivera <Andres.F.Rivera@jpl.nasa.gov>
2017-08-08 16:13:07 -0700

    better scope finding

--- editor/editor.js
+++ editor/editor.js
@@ -33,18 +33,20 @@ var kCompleter = {
             if(scope.length == 0){
                 scope = session.$struct;
             }
-            let topLevel = scope.children.filter(namedScopeFilter);
-            for (let elm of topLevel) {
-                keywords[elm.name] = elm.type;
-            }
-            if (prevTok && prevTok.type === "punctuation.member") {
-                let members = tokI - 2 > 0 ? findType(scope, toks[tokI - 2].value) : [];
-                for (let m of members) {
-                    members = members.concat(m.children.filter(k => k.type == 'function' || k.type == 'property')).filter(namedScopeFilter);
+            for(let s of scope){
+                let topLevel = s.children.filter(namedScopeFilter);
+                for (let elm of topLevel) {
+                    keywords[elm.name] = elm.type;
                 }
-                keywords = {};
-                for (let m of members) {
-                    keywords[m.name] = m.type;
+                if (prevTok && prevTok.type === "punctuation.member") {
+                    let members = tokI - 2 >= 0 ? findType(s, toks[tokI - 2].value) : [];
+                    for (let m of members) {
+                        members = members.concat(m.children.filter(k => k.type == 'function' || k.type == 'property')).filter(namedScopeFilter);
+                    }
+                    keywords = {};
+                    for (let m of members) {
+                        keywords[m.name] = m.type;
+                    }
                 }
             }
         }
@@ -65,43 +67,49 @@ var kCompleter = {
 };
 
 function findScope(node, tree){
-    const calcDistance = (p1,p2) => {
-        return {y: p1.start.line - p2.start.line, x: p1.start.col - p2.start.col};
-    };
-    const endDist = (p1,p2) => {
-        return {y: p1.end.line - p2.end.line, x: p1.end.col - p2.end.col};
-    };
-    const findMinScope = (scopes) => {
-        if (scopes.length === 0) {
-            return false;
-        }
-        const tDist = (x,y)=>Math.sqrt(Math.pow(x,2)+1000*Math.pow(y,2)); //hackish, line dist has superior weight
-        var min = scopes[0].scope;
-        var minDist = tDist(scopes[0].distance.x,scopes[0].distance.y);
-        for (var i = 1; i < scopes.length; i++) {
-            if(scopes[i].distance.y <= 0 && scopes[i].distance.x <=0){
-                continue; // it's outside of the scope
-            }
-            let dist = tDist(scopes[i].distance.x,scopes[i].distance.y);
-            if (dist < minDist) {
-                min = scopes[i].scope;
-                minDist = dist;
-            }
-        }
-        return min;
-    };
+    // const calcDistance = (p1,p2) => {
+    //     return {y: p1.start.line - p2.start.line, x: p1.start.col - p2.start.col};
+    // };
+    // const endDist = (p1,p2) => {
+    //     return {y: p1.end.line - p2.end.line, x: p1.end.col - p2.end.col};
+    // };
+    // const findMinScope = (scopes) => {
+    //     if (scopes.length === 0) {
+    //         return false;
+    //     }
+    //     const tDist = (x,y)=>Math.sqrt(Math.pow(x,2)+1000*Math.pow(y,2)); //hackish, line dist has superior weight
+    //     var min = scopes[0].scope;
+    //     var minDist = tDist(scopes[0].distance.x,scopes[0].distance.y);
+    //     for (var i = 1; i < scopes.length; i++) {
+    //         if(scopes[i].distance.y <= 0 && scopes[i].distance.x <=0){
+    //             continue; // it's outside of the scope
+    //         }
+    //         let dist = tDist(scopes[i].distance.x,scopes[i].distance.y);
+    //         if (dist < minDist) {
+    //             min = scopes[i].scope;
+    //             minDist = dist;
+    //         }
+    //     }
+    //     return min;
+    // };
+    const lt = (p1,p2) => (p1.line < p2.line) || (p1.line === p2.line) && (p1.col < p2.col);
 
     let scopes = dfs(tree[0],'child.children && child.children.length > 0',[]);
-    let distances = [];
-    for(let scope of scopes){
-        let endD = endDist(node,scope);
-        if(endD.y >= 0 && endD.x >= 0){
-            continue;
-        }
-        distances.push({scope: scope, distance: calcDistance(node,scope)});
-    }
-    let scope = findMinScope(distances);
-    return scope ? scope : tree[0];
+    let accessibleScopes = scopes
+                            .filter(scope => lt(scope.start, node.start))
+                            .filter(scope => lt(node.end, scope.end));
+
+    // let distances = [];
+    // for(let scope of scopes){
+    //     let endD = endDist(node,scope);
+    //     if(endD.y >= 0 && endD.x >= 0){
+    //         continue;
+    //     }
+    //     distances.push({scope: scope, distance: calcDistance(node,scope)});
+    // }
+    // let scope = findMinScope(distances);
+    // return scope ? scope : tree[0];
+    return accessibleScopes;
 
 }
 
